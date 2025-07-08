424a92a84f3cc85046aac61a7990ea49fd1d7756
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-08 09:50:03 +0300

    added the first code

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/home/home.dart';
+
+
+void main(){
+  runApp(
+    const Home(),
+  );
+}
+
+class SandBox extends StatelessWidget {
+  const SandBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text('Sandbox'),
+    );
+  }
+}
