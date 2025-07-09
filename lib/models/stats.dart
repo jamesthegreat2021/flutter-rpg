2d3f0c697d31aa17c1f7abeb0c0646f688430c99
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-09 09:59:01 +0300

    implemented the mixin for character class, no need for inheritence

---
+++ lib/models/stats.dart
@@ -0,0 +1,49 @@
+mixin Stats {
+int _points =10;
+int _health = 10;
+int _attack = 10; 
+int _defense =10;
+int _skills = 10;
+
+void incrementStats(String stat){
+  if (_points > 0){
+    if (stat == "health") {
+      _health++;
+    }
+    if (stat == "defense") {
+      _defense++;
+    }
+    if (stat == "attack") {
+      _attack++;
+    }
+    if (stat == "skills") {
+      _skills++;
+    } 
+  }
+}
+
+void decreaseStat(String stat){
+
+  if (stat == 'health' && _health > 5){
+    _health--;
+    _points++;
+  }
+  if (stat == 'defense' && _defense > 5){
+    _defense--;
+    _points++;
+  }
+  if (stat == 'attack' && _attack > 5){
+    _attack--;
+    _points++;
+  }
+  if (stat == 'skills' && _skills > 5){
+    _skills--;
+    _points++;
+  }
+
+}
+
+
+
+
+}
