40ff8073bbcbf94e0d72fb75a72499b6a60ec72c
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-10 17:01:19 +0300

    started working with user input

---
+++ lib/screens/create/create.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
+
+class Create extends StatefulWidget {
+  const Create({super.key});
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: StyledTitle('Character Creation'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        child: Column(
+          children: <Widget> [
+            Center(
+              child: Icon(Icons.code, color: AppColors.primaryColor,),
+
+            ),
+            Center(
+              child: StyledHeading('Welcome, new player'),
+            ),
+            const Center(
+              child: StyledText('Create the name & slogan for your character.'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
