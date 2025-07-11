3b79c428a08785c5f08eb5309781ef693f6e4872
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-11 08:55:17 +0300

    added scrolling capabilities

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -47,78 +47,80 @@ class _CreateState extends State<Create> {
         title: StyledTitle('Character Creation'),
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-        child: Column(
-          children: <Widget> [
-            Center(
-              child: Icon(Icons.code, color: AppColors.primaryColor,),
-
-            ),
-            Center(
-              child: StyledHeading('Welcome, new player'),
-            ),
-            const Center(
-              child: StyledText('Create the name & slogan for your character.'),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          child: Column(
+            children: <Widget> [
+              Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor,),
+        
+              ),
+              Center(
+                child: StyledHeading('Welcome, new player'),
+              ),
+              const Center(
+                child: StyledText('Create the name & slogan for your character.'),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+               TextField(
+                controller: _nameController,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+                cursorColor: AppColors.textColor,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person_2),
+                  label: StyledText('Character name'), 
+                ),
+              ),
+              const SizedBox(height: 20,),
              TextField(
-              controller: _nameController,
-              style: GoogleFonts.kanit(
-                textStyle: Theme.of(context).textTheme.bodyMedium,
+              controller: _sloganController,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium
+                ),
+                cursorColor: AppColors.textColor,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.chat), 
+                  label: StyledText('Character slogan'),
+                ),
               ),
-              cursorColor: AppColors.textColor,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person_2),
-                label: StyledText('Character name'), 
+              const SizedBox(height: 30),
+        
+               Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor,),
+        
               ),
-            ),
-            const SizedBox(height: 20,),
-           TextField(
-            controller: _sloganController,
-              style: GoogleFonts.kanit(
-                textStyle: Theme.of(context).textTheme.bodyMedium
+              Center(
+                child: StyledHeading('Choose a vocation'),
               ),
-              cursorColor: AppColors.textColor,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.chat), 
-                label: StyledText('Character slogan'),
+              const Center(
+                child: StyledText('This determines your available skills'),
               ),
-            ),
-            const SizedBox(height: 30),
-
-             Center(
-              child: Icon(Icons.code, color: AppColors.primaryColor,),
-
-            ),
-            Center(
-              child: StyledHeading('Choose a vocation'),
-            ),
-            const Center(
-              child: StyledText('This determines your available skills'),
-            ),
-            const SizedBox(height: 30,),
-
-            const VocationCard(
-               vocation: Vocation.junkie,
-            ),
-            const VocationCard(
-              vocation: Vocation.ninja,
-            ),
-            const VocationCard(
-              vocation: Vocation.raider,
-            ),
-            const VocationCard(
-              vocation: Vocation.wizard,
-            ),
-
-
-            StyledButton(
-              onPressed: (){}, 
-            child: StyledText('Create character')),
-          ],
+              const SizedBox(height: 30,),
+        
+              const VocationCard(
+                 vocation: Vocation.junkie,
+              ),
+              const VocationCard(
+                vocation: Vocation.ninja,
+              ),
+              const VocationCard(
+                vocation: Vocation.raider,
+              ),
+              const VocationCard(
+                vocation: Vocation.wizard,
+              ),
+        
+        
+              StyledButton(
+                onPressed: (){}, 
+              child: StyledText('Create character')),
+            ],
+          ),
         ),
       ),
     );
