258d5fe1583286c1b8870fb3f8165283d02be624
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-14 12:34:52 +0300

    added logics for serialization and deserialization of data towards and out of the database

--- lib/models/characters.dart
+++ lib/models/characters.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
 import 'package:flutter_rpg/models/vocation.dart';
@@ -32,18 +33,55 @@ void updateSkill(Skill skill){
 
 //character to firestore
 //recall the data needs to  be serialized to a certain format
-
+//this method can be called to any character instance
+//remember in dart the object is managed by dart
+//and it is being passed automatically as a reference
 Map<String, dynamic> toFirestore(){
   return {
     'name': name,
     'slogan': slogan,
     'isFav': _isFav,
     'vocation': vocation.toString(),
-    'skills': skills.map((s)=>s.id).toList(), 
+    'skills': skills.map((s)=> s.id).toList(), 
     'stats': statsAsMap, 
-    'points': points
+    'points': points, 
   };
 }
+
+
+//characrer from firestore
+factory Character.fromFirestore(
+  DocumentSnapshot<Map<String,dynamic>> snapshot,
+  SnapshotOptions options,
+)
+{
+  //get data from the snapshot
+  final data = snapshot.data()!; 
+
+  //making the character instance
+  Character character = Character(
+     name: data['name'],
+     slogan: data['slogan'],
+     id: snapshot.id,
+     vocation: Vocation.values.firstWhere((v)=> v.toString() == data['vocation'])
+); 
+   //updating the skills
+
+  for (String id in data['skills']){
+    Skill skill = allSkills.firstWhere((element) => element.id == id );
+    character.updateSkill(skill); 
+
+  }
+  //the last thing that is the set is fav 
+
+  if (data['isFav'] == true) {
+    character.toggleIsFav();
+  }
+
+
+  return character;
+
+
 
 
 }
