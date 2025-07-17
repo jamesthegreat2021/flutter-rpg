abb42b97701443fff02cd3277a4ee3959dc79322
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-17 11:17:47 +0300

    added the Hero built in widget for navigation between screens

--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -21,7 +21,9 @@ class CharacterCard extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Row(
           children: <Widget> [
-            Image.asset('assets/img/vocations/${character.vocation.image}', width: 80 ,),
+            Hero(
+              tag: character.id.toString(),
+              child: Image.asset('assets/img/vocations/${character.vocation.image}', width: 80 ,)),
 
             const SizedBox(width: 20),
             Column(
